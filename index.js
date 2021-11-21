df7439389d82f53eda5fba274053ede7b7e05a80
Avraham Avrahami <avr248@gmail.com>
2021-11-21 10:45:56 +0200

    Fix resolve mongodb

--- index.js
+++ index.js
@@ -27,14 +27,7 @@ export const getBooleanIfValid = (value, defaultValue = null) => {
 		return typeof value === 'boolean' ? value : defaultValue;
 	}
 };
-export const getObjectIDIfValid = value => {
-    try {
-        const mongodb = require('mongodb');
-	    return mongodb && mongodb.ObjectID.isValid(value) ? new ObjectID(value) : null;
-    } catch (error) {
-        console.log(error)
-    }
-};
+
 export const getBrowser = browser => {
 	return browser
 		? {
@@ -82,53 +75,7 @@ export const getCustomerAddress = address => {
     }
     
 };
-export const getOrderAddress = address => {
-	let coordinates = {
-		latitude: '',
-		longitude: ''
-	};
 
-	if (address && address.coordinates) {
-		coordinates.latitude = address.coordinates.latitude;
-		coordinates.longitude = address.coordinates.longitude;
-	}
-
-	const emptyAddress = {
-		full_name: '',
-		address1: '',
-		address2: '',
-		city: '',
-		country: '',
-		postal_code: '',
-		state: '',
-		phone: '',
-		company: '',
-		tax_number: '',
-		coordinates: coordinates,
-		details: null
-	};
-
-	return address
-		? Object.assign(
-				{},
-				{
-					full_name: getString(address.full_name),
-					address1: getString(address.address1),
-					address2: getString(address.address2),
-					city: getString(address.city),
-					country: getString(address.country).toUpperCase(),
-					postal_code: getString(address.postal_code),
-					state: getString(address.state),
-					phone: getString(address.phone),
-					company: getString(address.company),
-					tax_number: getString(address.tax_number),
-					coordinates: coordinates,
-					details: address.details
-				},
-				address
-		  )
-		: emptyAddress;
-};
 export const basename = function(path) {
    return path.split('/').reverse()[0];
 };
@@ -317,6 +264,61 @@ const calc = {
 export const cl = function(msg) {
     console.log('msg', msg)
 }
+const getObjectIDIfValid = value => {
+    try {
+        const mongodb = require('mongodb');
+	    return mongodb && mongodb.ObjectID.isValid(value) ? new ObjectID(value) : null;
+    } catch (error) {
+        console.log(error)
+    }
+};
+const getOrderAddress = address => {
+	let coordinates = {
+		latitude: '',
+		longitude: ''
+	};
+
+	if (address && address.coordinates) {
+		coordinates.latitude = address.coordinates.latitude;
+		coordinates.longitude = address.coordinates.longitude;
+	}
+
+	const emptyAddress = {
+		full_name: '',
+		address1: '',
+		address2: '',
+		city: '',
+		country: '',
+		postal_code: '',
+		state: '',
+		phone: '',
+		company: '',
+		tax_number: '',
+		coordinates: coordinates,
+		details: null
+	};
+
+	return address
+		? Object.assign(
+				{},
+				{
+					full_name: getString(address.full_name),
+					address1: getString(address.address1),
+					address2: getString(address.address2),
+					city: getString(address.city),
+					country: getString(address.country).toUpperCase(),
+					postal_code: getString(address.postal_code),
+					state: getString(address.state),
+					phone: getString(address.phone),
+					company: getString(address.company),
+					tax_number: getString(address.tax_number),
+					coordinates: coordinates,
+					details: address.details
+				},
+				address
+		  )
+		: emptyAddress;
+};
 export default {
     basename,
 	getString,
